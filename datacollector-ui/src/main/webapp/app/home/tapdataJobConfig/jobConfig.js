10d68526314b91402a7d83e65494c0461f4830a1
Waiter Zen <waiter.james1991@gmail.com>
2018-05-20 16:18:20 +0800

    [frontend] update config

--- datacollector-ui/src/main/webapp/app/home/tapdataJobConfig/jobConfig.js
+++ datacollector-ui/src/main/webapp/app/home/tapdataJobConfig/jobConfig.js
@@ -4,7 +4,7 @@ angular
   .controller('jobConfigController', function ($scope, pipelineService, pipelineConstant) {
    
     $scope.$watch('pipelineConfig.metadata.tapdata_syncDate', function() {
-        if(typeof typeof $scope.pipelineConfig.metadata.tapdata_syncDate === 'string'){
+        if(typeof $scope.pipelineConfig.metadata.tapdata_syncDate === 'string'){
             $scope.pipelineConfig.metadata.tapdata_syncDate =  new Date($scope.pipelineConfig.metadata.tapdata_syncDate)
         }
       
